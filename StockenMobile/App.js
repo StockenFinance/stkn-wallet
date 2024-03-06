1f1b3237b1d4c885c22a137e5c8451ce8877a62b
Mohd Aasim <mohd.aasim@oodles.io>
2024-03-06 12:43:55 +0530

    Complted backup phrase to store UI

--- StockenMobile/App.js
+++ StockenMobile/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import Welcome from './src/Welcome/Welcome';
 import ImportWallet from './src/ImportWallet/ImportWallet';
 import CreateWallet from './src/CreateWallet/CreateWallet';
+import BackupPhrase from './src/BackupPhrase/BackupPhrase';
 
 const App = () => {
-  return <ImportWallet />;
+  return <BackupPhrase />;
 };
 
 export default App;
