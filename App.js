81eeb1a17b37479a41dae944658a41fe4b2e9c84
Mohd Aasim <mohd.aasim@oodles.io>
2024-03-12 19:02:56 +0530

    changed folder structure

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+// import Welcome from './src/Welcome/Welcome';
+// import ImportWallet from './src/ImportWallet/ImportWallet';
+// import CreateWallet from './src/CreateWallet/CreateWallet';
+// import BackupPhrase from './src/BackupPhrase/BackupPhrase';
+// import ConfirmBackupPhrase from './src/ConfirmBackupPhrase/ConfirmBackupPhrase';
+// import RecoveryPhraseConfirmation from './src/RecoveryPhraseConfirmation/RecoveryPhraseConfirmation';
+// import Dashboard from './src/Dashboard/Dashboard';
+// import SwapScreen from './src/SwapScreen/SwapScreen';
+// import Settings from './src/Settings/Settings';
+// import SendScreen from './src/SendScreen/SendScreen';
+// import BuyScreen from './src/BuyScreen/BuyScreen';
+import ChartScreen from "./src/ChartScreen/ChartScreen";
+import Activity from "./src/Activity/Activity";
+
+const App = () => {
+  return <Activity />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
